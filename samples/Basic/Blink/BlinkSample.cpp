978f3aa18985701d5e82a16361b6b8f655d6ec70
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-08-25 13:00:44 +0000

    Fix sample error

--- samples/Basic/Blink/BlinkSample.cpp
+++ samples/Basic/Blink/BlinkSample.cpp
@@ -4,8 +4,7 @@ void BlinkSample_main(codal::STM32NUCLEO_F4x1RE& nucleoBoard)
 {
     bool state = false;
     while (true) {
-        nucleoBoard.io.led1.setDigitalValue((int)state);
-        nucleoBoard.io.led2.setDigitalValue((int)!state);
+        nucleoBoard.io.led.setDigitalValue((int)state);
         nucleoBoard.sleep(1000);
         state = !state;
     }
